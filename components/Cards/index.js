24279c678c0113507e96798fa2378e3b678db7b4
lyndsiWilliams <kcatgirl@gmail.com>
2019-11-01 13:00:03 -0500

    Almost done! Yet another pointing issue...

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,56 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+// HTTP GET request
+axios.get(`https://lambda-times-backend.herokuapp.com/articles`)
+    .then(response => {
+        // console.log(response.data.articles.javascript);
+        const newCard = createCard(response.data.articles.javascript);
+        document.querySelector('.cards-container').appendChild(newCard);
+    })
+
+
+// Grab parent element
+// const cards = document.querySelector('.cards-container');
+
+// Append (Loop through data)
+// cards.appendChild(createCard(getCard));
+
+// Component function
+function createCard(headline, image, name) {
+
+    // Element definition
+    const card = document.createElement('div');
+    const headlineDiv = document.createElement('div');
+    const author = document.createElement('div');
+    const imgCont = document.createElement('div');
+    const img = document.createElement('img');
+    const authorName = document.createElement('span');
+
+
+    // Appending children to their parents
+    card.append(headlineDiv, author);
+    author.append(imgCont, authorName);
+    imgCont.appendChild(img);
+
+
+    // Class names
+    card.classList.add('card');
+    headlineDiv.classList.add('headline');
+    author.classList.add('author');
+    imgCont.classList.add('img-container');
+
+
+    // Content
+    headlineDiv.textContent = headline;
+
+    // img.src = headline.authorPhoto;
+    // authorName.textContent = getCard.data.authorName;
+    // console.log(getCard.data.articles.javascript);
+
+
+
+    return card;
+}
